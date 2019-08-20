33e59f259512be98c0e72bf2dc32c28048c6c02d
sfn <safdarh@carleton.edu>
2019-08-20 17:08:05 +0500

    slightly improved, cleaner code, still no working diffuse

--- Assets/Scripts/LightRays.cs
+++ Assets/Scripts/LightRays.cs
@@ -23,10 +23,10 @@ public class LightRays : MonoBehaviour
 
     float totalWidth = 0.05f;
 
-    GameObject specular;
-    // specular lines
-    GameObject[] extraLines;
+    GameObject[] specularLines;
     GameObject[] diffuseLines;
+    // whether it is rendered or not
+    bool diffReflections = true, specReflections = true;
 
     float metalness;
     float smoothness;
@@ -34,12 +34,13 @@ public class LightRays : MonoBehaviour
     void Start()
     {
         lightPoint = GameObject.Find("Spot Light").transform.position;
-        // old method using fixed reflectionpoint set in scene
-        //reflectionPoint = GameObject.Find("reflectionpoint").transform.position;
 
         // call function with light point and direction of light
         findReflectionPoint(lightPoint, GameObject.Find("Spot Light").transform.forward);
+        
         incoming = reflectionPoint - lightPoint;
+        // only needs to be recalculated after map change
+        reflection = reflectionPoint + Vector3.Reflect(incoming, actualNormal);
 
         // draw incoming line
         var line = gameObject.AddComponent<LineRenderer>();
@@ -75,68 +76,54 @@ public class LightRays : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(incRay, out hit, 2.0f)) {
             // save normal vector and reflection point
-            // normal = hit.normal;
             reflectionPoint = hit.point;
             actualNormal = hit.normal;
 
+            // find normal based on texcoords
             Vector2 texCoord = hit.textureCoord;
             Texture2D normalTex = 
                 (Texture2D)GameObject.Find("Sphere").GetComponent<MeshRenderer>().material.GetTexture("_NormalMap");
             normal.x = normalTex.GetPixelBilinear(texCoord.x, texCoord.y).r;
             normal.y = normalTex.GetPixelBilinear(texCoord.x, texCoord.y).g;
             normal.z = normalTex.GetPixelBilinear(texCoord.x, texCoord.y).b;
-
-            // only needs to be done once
-            incoming = reflectionPoint - pos;
-            // only needs to be recalculated after map change
-            reflection = reflectionPoint + Vector3.Reflect(incoming, actualNormal);
-
-            //normal = Vector3.Normalize(normal);
         }
     }
 
     public void drawSpecular() {
-        specular = new GameObject("specular ray");
-        var specLine = specular.AddComponent<LineRenderer>();
-        specLine.positionCount = 2;
-        // assumes that width is metalness fraction
-        specLine.startWidth = totalWidth * metalness;
-        specLine.SetPosition(0, reflectionPoint);
-        specLine.SetPosition(1, reflection);
-        specLine.useWorldSpace = false;
-        specLine.material = specMat;
-
-        // Debug.Log(normalOffsetAngle);
-
-        Destroy(specular);
-
         // slightly higher than just metalness as there is always spec
         // yes i break the physically based part of PBR
-        float specWidth = totalWidth * metalness + totalWidth * 0.1f;
+        float specWidth = 2 * (totalWidth * metalness + totalWidth * 0.1f);
         // 20 rays at starting intensity
-        int numberOfSpecRays = (int) (specWidth * 400f);
+        int numberOfSpecRays = (int) (specWidth * 600f);
+
+        specularLines = new GameObject[numberOfSpecRays];
 
         // draw specular lines with normal and smoothness offsets
-        extraLines = new GameObject[numberOfSpecRays];
         for(int i = 0; i < numberOfSpecRays; i++) {
-            extraLines[i] = Instantiate(specular);
-            // factor of ten based on width
-            extraLines[i].GetComponent<LineRenderer>().startWidth = specWidth / numberOfSpecRays;
+            specularLines[i] = new GameObject("specular ray");
+
+            // making the base line to later rotate
+            var line = specularLines[i].AddComponent<LineRenderer>();
+            line.positionCount = 2;
+            line.startWidth = specWidth / numberOfSpecRays;
+            line.SetPosition(0, reflectionPoint);
+            line.SetPosition(1, reflection);
+            line.useWorldSpace = false;
+            line.material = specMat;
 
             // factoring in smoothness, smoother should result in denser highlight
             float minTilt = -normalOffsetAngle - (float)System.Math.Pow(5 * (1 - smoothness), 2);
             float maxTilt = normalOffsetAngle + (float)System.Math.Pow(5 * (1 - smoothness), 2);
-            float[] tilt = {
+            specularLines[i].GetComponent<Transform>().Rotate(
                 Random.Range(minTilt, maxTilt), 
                 Random.Range(minTilt, maxTilt), 
-                Random.Range(minTilt, maxTilt)};
-            extraLines[i].GetComponent<Transform>().Rotate(tilt[0], tilt[1], tilt[2]);
+                Random.Range(minTilt, maxTilt));
         }
     }
 
     public void drawDiffuse() {
         // at max metalness, diffuse doesn't exist
-        float diffWidth = totalWidth - totalWidth * metalness;
+        float diffWidth = 2 * (totalWidth - totalWidth * metalness);
         // tbh idk how many rays this is but should be enough
         int numberOfDiffRays = (int) (diffWidth * 600f);
 
@@ -145,6 +132,7 @@ public class LightRays : MonoBehaviour
         for(int i = 0; i < numberOfDiffRays; i++) {
             diffuseLines[i] = new GameObject("diffuse ray");
 
+            // makes the base line
             var line = diffuseLines[i].AddComponent<LineRenderer>();
             line.positionCount = 2;
             line.startColor = Color.red;
@@ -157,24 +145,42 @@ public class LightRays : MonoBehaviour
         }
     }
 
-    void destroyAndRefresh() {
-        // self explanatory tbh
-        foreach(GameObject s in extraLines) {
-            Destroy(s);
-        }
-        drawSpecular();
-
-        foreach(GameObject g in diffuseLines) {
-            Destroy(g);
-        }
-        drawDiffuse();
-    }
-
-    void Update() {
+    // public so it can be called after any UI change
+    public void destroyAndRefresh() {
         // offsetting with normal map
         // angle with 128 128 256 as that is base in normal map
         normalOffsetAngle = Vector3.Angle(normal, new Vector3(128, 127, 255));
 
         findReflectionPoint(lightPoint, GameObject.Find("Spot Light").transform.forward);
+
+        // self explanatory tbh
+        foreach(GameObject s in specularLines) {
+            Destroy(s);
+        }
+        if(specReflections) {
+            drawSpecular();
+        }
+
+        foreach(GameObject g in diffuseLines) {
+            Destroy(g);
+        }
+        if(diffReflections) {
+            drawDiffuse();
+        }
+    }
+
+    public void toggleDiffuse(bool toggle) {
+        diffReflections = toggle;
+        destroyAndRefresh();
+    }
+
+    public void toggleSpecular(bool toggle) {
+        specReflections = toggle;
+        destroyAndRefresh();
+    }
+
+    public void toggleNormals() {
+        reflection = reflectionPoint + Vector3.Reflect(incoming, actualNormal);
+        destroyAndRefresh();
     }
 }
